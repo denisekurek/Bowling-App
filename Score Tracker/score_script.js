90cd2c5615f704ad8687a406c093ccb6241eedb4
Denise Kurek <denisekurek@rocketmail.com>
2020-09-28 16:39:44 -0400

    Added first bit of functionality for pins with javascript. Added more space between navigation tiles. Commented out parts of score tracking that I am not working on.

--- Score Tracker/score_script.js
+++ Score Tracker/score_script.js
@@ -1,1 +1,16 @@
-hi
+let pins = document.querySelectorAll('.pin');
+
+function hidePin(e) {
+  // console.log(e);
+  if (pin.style.opacity === "1")
+  {
+    pin.style.opacity = "0";
+  }
+  else {
+    pin.style.opacity = "1";
+  }
+}
+
+pins.forEach(function(element) {
+  element.addEventListener('click', hidePin);
+});
