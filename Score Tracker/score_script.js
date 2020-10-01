9bb8141c2d51a22b9655f5fcd98b4c3b294231f2
Denise Kurek <denisekurek@rocketmail.com>
2020-10-01 16:32:06 -0400

    JS from previous request.

--- Score Tracker/score_script.js
+++ Score Tracker/score_script.js
@@ -1,19 +1,41 @@
-let pins = document.querySelectorAll('.pin_box');
-let count = 0;
+// let pin1 = document.querySelector('#pin1');
+// let pin2 = document.querySelector('#pin2');
+// let pin3 = document.querySelector('#pin3');
+// let pin4 = document.querySelector('#pin4');
+// let pin5 = document.querySelector('#pin5');
+// let pin6 = document.querySelector('#pin6');
+// let pin7 = document.querySelector('#pin7');
+// let pin8 = document.querySelector('#pin8');
+// let pin9 = document.querySelector('#pin9');
+// let pin10 = document.querySelector('#pin10');
+let pin = document.querySelectorAll('.pin')
+let count = 10;
 let bowling = false;
 
 function hidePin(e) {
   // console.log(e);
-
-  if (pins.style.display == "inline-flex")
-  {
-    pins.style.display = "none";
+  console.log(count);
+  if (this.style.opacity == "0") {
+    this.setAttribute('style','opacity:1');
+    count++;
   }
   else {
-    pins.style.display = "inline-flex";
+    this.style.opacity = "0";
+    count--;
   }
 }
 
-pins.forEach(function(element) {
+// pin1.addEventListener('click', hidePin)
+// pin2.addEventListener('click', hidePin)
+// pin3.addEventListener('click', hidePin)
+// pin4.addEventListener('click', hidePin)
+// pin5.addEventListener('click', hidePin)
+// pin6.addEventListener('click', hidePin)
+// pin7.addEventListener('click', hidePin)
+// pin8.addEventListener('click', hidePin)
+// pin9.addEventListener('click', hidePin)
+// pin10.addEventListener('click', hidePin)
+
+pin.forEach(function(element) {
   element.addEventListener('click', hidePin);
 });
